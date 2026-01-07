ec4479b64a949e5e73d14a0dba2994a5cd54a162
Magesh Kuppan <tkmagesh77@gmail.com>
2026-01-07 15:16:30 +0530

    fmt-scan

---
+++ 04-fmt-scan/01-demo.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		var username string
+		fmt.Println("Enter your name :")
+		fmt.Scanln(&username)
+		fmt.Println("Entered name :", username)
+	*/
+
+	var firstName, lastName string
+	fmt.Println("Enter your complete name:")
+	fmt.Scanln(&firstName, &lastName)
+	// fmt.Println(lastName, ", ", firstName)
+	fmt.Printf("%d, %s\n", lastName, firstName)
+
+}
