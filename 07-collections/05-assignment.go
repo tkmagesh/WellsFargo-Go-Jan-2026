7455c1758600bac92cfb9127e134b961901c34ba
Magesh Kuppan <tkmagesh77@gmail.com>
2026-01-08 14:02:08 +0530

    collections assignment

---
+++ 07-collections/05-assignment.go
@@ -0,0 +1,14 @@
+/*
+Find the most occurring "word size" in the given string and print the most occuring "word size" & the number of occurances
+Use "strings.Split()" to get the words
+Use "len()" to get the length of a word
+*/
+/*
+	ex output format:
+	5 letter words occurs the most with 31 occurrances
+*/
+package main
+
+func main() {
+	str := "Commodo Lorem enim dolore culpa minim ipsum ex excepteur in Commodo duis nulla ex laborum irure sunt incididunt Incididunt amet Lorem amet dolor sit consectetur culpa esse quis laborum pariatur laborum fugiat mollit Mollit voluptate aliquip Lorem incididunt mollit pariatur eu enim proident culpa esse laborum voluptate Nostrud aliqua magna ipsum qui duis euNisi pariatur sit do magna Lorem nostrud voluptate occaecat occaecat quis dolore irure Velit aliqua reprehenderit eu duis aliqua excepteur duis non enim Nostrud qui voluptate enim eiusmod dolor proident laboris nostrud commodo laborum aliquip sunt Exercitation do anim do ullamco Ipsum eiusmod aute qui ea consectetur Veniam laborum occaecat mollit pariatur commodo id ullamco dolore ipsum sit dolore elit fugiatUt ad aliqua dolor quis velit reprehenderit Dolore aliquip exercitation do fugiat Pariatur irure aliqua magna quis"
+}
