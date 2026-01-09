c9b611f2311b78b7f20b736f5fe8320c2d6fb617
Magesh Kuppan <tkmagesh77@gmail.com>
2026-01-09 11:42:14 +0530

    OOP assignment

---
+++ 12-methods/03-assignment.go
@@ -0,0 +1,9 @@
+
+/*
+Create a type to represent a ShoppingCart
+	- should be able to add products to the cart
+	- each product added to the cart must have "quantity" as well
+	- should be able to apply discounts to the products in the cart
+	- should be able to list the products & their item values
+	- should be able to print the overall cart value
+*/
